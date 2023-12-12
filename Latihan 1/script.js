aa64b43d8eae965bdaaa550310da7fa3ebd1d389
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-12 09:58:20 +0700

    Latihan part 2

---
+++ Latihan 1/script.js
@@ -0,0 +1,34 @@
+//Latihan menampilkan output
+console.log('Namaku Faiz');
+document.write('Ini adalah latihan pertama : output dan variable');
+document.write('<hr>');
+document.write("<br>");
+let x=10;
+let y=5;
+document.write('10 + 5 = ' + (x+y));
+document.write("<br>");
+document.write('10 - 5 = ' + (x-y));
+document.write("<br>");
+document.write('10 x 5 = ' + (x*y));
+document.write("<br>");
+document.write('10 : 5 = ' + (x/y));
+document.write("<br>");
+document.write('10^5 = ' + (x**y));
+document.write("<br>");
+document.write('Nilai x berubah menjadi 20');
+document.write("<br>");
+document.write("<br>");
+x=20;
+document.write('x + y = ' + (x+y))
+document.write("<br>");
+document.write("<br>");
+
+{
+    document.write('Deklarasi ulang x di dalam blok');
+    document.write("<br>");
+    let x = 11;
+    document.writeln('x = ' + x)
+}
+document.write("<br>");
+document.write("<br>");
+document.writeln('Nilai x di luar blok = ' + x)
