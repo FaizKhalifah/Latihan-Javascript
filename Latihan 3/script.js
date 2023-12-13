6e453db29b0a5ecea0b755b6dd7d976c31b9387b
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-13 22:42:45 +0700

    Latihan 3, latihan objek

---
+++ Latihan 3/script.js
@@ -0,0 +1,39 @@
+//Latihan object
+const mahasiswa_1={
+    nama:"Faiz",
+    asal:"Bekasi",
+    fakultas:"Ilmu Komputer",
+    prodi:"Teknik informatika",
+    jalur:"mandiri"
+};
+
+//Biasanya, object menggunakan variable const
+
+const mahasiswa_2={
+    nama:"Ravi",
+    asal:"Yogyakarta",
+    fakultas:"Ilmu komputer",
+    prodi:"Teknik informatika",
+    jalur:"mandiri"
+}
+
+document.write(mahasiswa_1.nama);
+document.write("<br>")
+
+document.write(mahasiswa_1.asal);
+document.write("<br>")
+
+document.write(mahasiswa_1.prodi);
+document.write("<br>")
+
+
+document.write(mahasiswa_2.nama);
+document.write("<br>")
+
+
+document.write(mahasiswa_2.asal);
+document.write("<br>")
+
+
+document.write(mahasiswa_2.prodi);
+document.write("<br>")
