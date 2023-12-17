8aa3a29d0ff7c6ed6377257f4193dabf46ecfe28
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-17 13:28:10 +0700

    Latihan 6 part 1, if else

---
+++ Latihan 6/script.js
@@ -0,0 +1,11 @@
+//Latihan if else
+let nilai =40;
+if(nilai>80){
+    document.write('Selamat kamu mendapat nilai A');
+} else if(nilai>70){
+    document.writeln('Kamu mendapat nilai B, pertahankan');
+}
+else{
+    document.writeln('Coba lagi di kesempatan berikutnya');
+}
+
