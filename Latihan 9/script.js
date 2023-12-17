0c0241fd52786ff1b3a1b91f9eac318a167a351b
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-17 17:00:31 +0700

    Latihan 9, for in loop

---
+++ Latihan 9/script.js
@@ -0,0 +1,13 @@
+const mahasiswa=["Faiz", "Ravi", "Yasin", "Zufar", "Bagas", "Ghaza"];
+for(i in mahasiswa){
+    document.write(mahasiswa[i]);
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+const nama_lengkap=["Salsa", "Zufar", "Radinka", "Akmal"];
+var nama="";
+for(i in nama_lengkap){
+    nama+=nama_lengkap[i] + " ";
+}
+document.write(nama);
