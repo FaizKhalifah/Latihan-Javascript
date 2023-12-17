09f2b3c2126115d4faefa46ed470ac2a1c40282a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-17 13:33:12 +0700

    Latihan 6 part 2, switch case

--- Latihan 6/script.js
+++ Latihan 6/script.js
@@ -9,3 +9,21 @@ else{
     document.writeln('Coba lagi di kesempatan berikutnya');
 }
 
+document.write("<br>");
+
+//Latihan Switch
+let semester = 4;
+    switch(semester){
+        case 1 :
+            document.write('Kamu adalah mahasiswa semester 1');
+            break;
+        case 2 :
+            document.writeln('Kamu adalah mahasiswa semester 2');
+            break;
+        case 3 : 
+            document.writeln('Kamu adalah mahasiswa semester 3');
+            break;
+        case 4 :
+            document.writeln('Kamu adalah mahasiswa semester 4');
+            break;
+    }
