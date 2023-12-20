9df9f63cfa7f1ecf79584f3b97c40f601f855b63
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-20 20:39:48 +0700

    Latihan 15, arrow function

---
+++ Latihan 15/script.js
@@ -0,0 +1,13 @@
+//Latihan arrow function
+let pertambahan = (x,y) => x+y;
+let pengurangan=(x,y)=>x-y;
+let perkalian=(x,y)=>x*y;
+let pembagian=(x,y)=>x/y;
+document.write(pertambahan(9,5));
+document.write("</br>");
+document.write(pengurangan(18,11));
+document.write("</br>");
+document.write(perkalian(5,11));
+document.write("</br>");
+document.write(pembagian(100,25));
+
