5e55558b94f7cae4f800f5138d842208dffc30e9
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-23 06:15:18 +0700

    latihan tiga DOM, mengubah elemen

---
+++ Latihan DOM/Latihan 3/script.js
@@ -0,0 +1,5 @@
+let matkul = document.querySelector("ul");
+let container = document.querySelector(".container");
+matkul.innerHTML+="<li>Keamanan informasi</li>";
+matkul.innerHTML+="<li>Analisis dan Perancangan Sistem</li>";
+container.innerHTML+="<h1>Itulah mata kuliah semester empat</h1>";
