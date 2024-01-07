a9e7dc9ac11801d84732a0c671098603b3dca8d9
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-07 21:58:56 +0700

    Latihan dasar 18, function lanjut

---
+++ Latihan Dasar/Latihan 18/script.js
@@ -0,0 +1,25 @@
+const fungsi1 = function(a, b){
+    return a+b;
+}
+
+document.write(fungsi1(5,7));
+document.write("<br>");
+
+
+//self invoke
+(function(){
+    document.write("Ini adalah fungsi self invoke");
+    document.write("<br>");
+})();
+
+
+//arrow function
+const fungsi2 = (a,b)=>a*b;
+
+// Sama saja dengan var x = function(a,b){
+//     return a*b;
+// }
+
+document.write(fungsi2(5,10));
+document.write("<br>");
+
