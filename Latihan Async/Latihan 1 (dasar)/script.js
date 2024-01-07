327abc1025342cbfd2390d3f3bd9e2c5f56a13a5
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-07 11:15:57 +0700

    Latihan async 1 , dasar async

---
+++ Latihan Async/Latihan 1 (dasar)/script.js
@@ -0,0 +1,17 @@
+document.write(1);
+document.write("<br>")
+document.write(2);
+document.write("<br>")
+document.write(3);
+
+//fungsi async
+setTimeout(()=>{
+    document.write("Program berhasil dijalankan");
+},2000); //dieksekusi setelah 2000 ms (2 detik)
+
+
+document.write("<br>")
+document.write(4);
+document.write("<br>")
+document.write(5);
+document.write("<br>");
