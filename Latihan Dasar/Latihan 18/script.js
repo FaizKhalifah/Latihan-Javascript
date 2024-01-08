fae7753c057cd68488ec52400df669022f848949
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-08 09:29:53 +0700

    Latihan dasar 18, function call

--- Latihan Dasar/Latihan 18/script.js
+++ Latihan Dasar/Latihan 18/script.js
@@ -23,3 +23,16 @@ const fungsi2 = (a,b)=>a*b;
 document.write(fungsi2(5,10));
 document.write("<br>");
 
+//function call
+const orang = {
+    namaLengkap:function(){
+        document.write(`${this.namaAwal} ${this.namaAkhir} <br>`);
+    }
+}
+
+const orang1={
+    namaAwal:"Faiz",
+    namaAkhir:"Alamsyah"
+}
+
+orang.namaLengkap.call(orang1);
